d39d3634ca761aca91e89ee4a50c8409248822e0
Chanjoo lee <leecj0805@naver.com>
2021-04-16 15:01:35 +0900

    리듀서 추가

--- src/TodoContext.js
+++ src/TodoContext.js
@@ -1,3 +1,4 @@
+import { parseSync } from '@babel/core';
 import React, {useReducer} from 'react';
 
 const initialTodos = [
@@ -22,3 +23,24 @@ const initialTodos = [
     done: false
   }
 ];
+
+
+function todoReducer(state, action) {
+  switch (action.type) {
+    case 'CREATE':
+      return state.concat(action.todo);
+    case 'TOGGLE':
+      return state.map(todo =>
+        todo.id === action.id ? {...todo, done:!todo.done} : todo
+      );
+    case 'REMOVE':
+      return state.filter(todo => todo.id !== action.id);
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+}
+
+export function TodoProvider({ children }){
+  const [state, dispatch] = useReducer(todoReducer, initialTodos);
+  return children;
+}
