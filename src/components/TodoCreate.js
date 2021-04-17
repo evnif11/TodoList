06e487e2e6f8542b6c1381e4654b09d3d033aa90
Chanjoo lee <leecj0805@naver.com>
2021-04-17 16:53:39 +0900

    태스크 생성 기능 구현

--- src/components/TodoCreate.js
+++ src/components/TodoCreate.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { MdAdd } from "react-icons/md";
 import styled, { css } from "styled-components";
+import { useTodoDispatch, useTodoNextId } from "../TodoContext";
 
 
 const CreateButton = styled.button`
@@ -74,14 +75,40 @@ const Input = styled.input`
 
 const TodoCreate = () => {
   const [ open, setOpen ] = useState(false);
+  const [ value, setValue ] = useState('');
+
+  const dispatch = useTodoDispatch();
+  const nextId = useTodoNextId();
+
+
   const onToggle = () => setOpen(!open);
+  const onChange = e => setValue(e.target.value);
+  const onSubmit = e => {
+    e.preventDefault();
+    dispatch({
+      type:'CREATE',
+      todo: {
+        id: nextId.current,
+        text: value,
+        done: false
+      }
+    });
+    setValue('');
+    setOpen(false);
+    nextId.current += 1;
+  };
 
   return (
     <div>
       { open && (
         <InsertFormPositioner>
-        <InsertForm>
-          <Input autoFocus placeholder="할 일을 입력 후 Enter를 누르세요"></Input>
+        <InsertForm onSubmit={onSubmit}>
+          <Input
+            autoFocus
+            placeholder="할 일을 입력 후 Enter를 누르세요"
+            onChange={onChange}
+            value={value}
+          />
         </InsertForm>
       </InsertFormPositioner>
       )}
@@ -93,4 +120,4 @@ const TodoCreate = () => {
   );
 }
 
-export default TodoCreate;
+export default React.memo(TodoCreate);
